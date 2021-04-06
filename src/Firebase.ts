3270cada23d3a3bb53b7d3d9ac0a00ee27ba40ac
DarkCh4osss <67869598+DarkCh4osss@users.noreply.github.com>
2021-04-06 22:50:46 +0200

    First Commit

--- src/Firebase.ts
+++ src/Firebase.ts
@@ -3,14 +3,8 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBYj1MMBttkxDZ6DsBLDNrzAlVNgVh54b8",
-    authDomain: "test-slot-mac-a358f.firebaseapp.com",
-    projectId: "test-slot-mac-a358f",
-    storageBucket: "test-slot-mac-a358f.appspot.com",
-    messagingSenderId: "697366630767",
-    appId: "1:697366630767:web:c8d79c3dbc44bfe57d589a",
-    measurementId: "G-LW8H79WH4F"
-}
+    //your config
+  };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 

a12db37027ba468d87b04394c0d94d7210fef4de
DarkCh4osss <67869598+DarkCh4osss@users.noreply.github.com>
2021-04-06 18:24:05 +0200

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,12 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import Slot from './components/Slot';
 import TitleBar from './components/TitleBar';
 import Login from './pages/Login';
-import { useSelector, useDispatch } from 'react-redux';
-import { login, logout, selectUser } from './features/isLogged';
 import { auth } from './Firebase';
+import {useAuthState} from 'react-firebase-hooks/auth';
 
 function App() {
-  const dispatch = useDispatch();
-  const user = useSelector(selectUser);
-
-  useEffect(() => {
-    auth.onAuthStateChanged((authUser) => {
-      if(authUser) {
-        dispatch(login({
-          uid: authUser.uid,
-          photo: authUser.photoURL,
-          email: authUser.email,
-          displayName: authUser.displayName,
-        }));
-      } else {
-        dispatch(logout());
-      }
-    })
-  }, [dispatch])
+  const [user] = useAuthState(auth);
 
   return (
     <>
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './store/index'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
